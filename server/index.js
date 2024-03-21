de7c35f9c35b7c57eb3d6d3d6a2214e9c6e7af94
Jenny Zhang <jenny@Jennys-MacBook-Pro.local>
2024-03-21 15:15:25 -0400

    test deployment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: '2024!'});
 });
 
 const PORT = process.env.PORT || 5000;
